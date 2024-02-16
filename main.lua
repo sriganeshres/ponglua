736008cd6b0d8cbe7115359b096500d48817e226
sriganeshres <sriganeshthota12345@gmail.com>
2024-02-17 00:09:15 +0530

    Resize Update

--- main.lua
+++ main.lua
@@ -36,11 +36,15 @@ function love.load()
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 
     gameState = 'start'
+end
+
+function resize(w, h)
+    push:resize(w, h)
 end
 
 function love.update(dt)
